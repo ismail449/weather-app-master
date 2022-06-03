3acb015d75e9511677d45b97805ca6a37a4a6bae
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-06-03 21:55:00 +0200

    fix

--- src/apiKey.js
+++
@@ -1,3 +0,0 @@
-export const key = 'appid=7b7533e428ca07a3b76f206929650e43&units=metric';
-export const cityAutoCompleteKey = 'RVDBZeABTup2lYufOQFZeRpKShDgRFsR';
-export const url = 'https://api.openweathermap.org/data/2.5/';
